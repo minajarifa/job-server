f24699dc2ba9bab796246c4e7d798f68009bb0ab
Your Name <minajmiha02@gmail.com>
2025-07-13 12:28:05 +0600

    job data get by id

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const jobCollection = client.db("jobPortal").collection("jobs");
     app.get("/jobs", async (req, res) => {
@@ -34,7 +34,7 @@ async function run() {
       const result = await jobCollection.findOne(query);
       res.send(result);
     });
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
