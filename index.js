af2e393b48a80c343361c4fce103771acd8b3613
Your Name <minajmiha02@gmail.com>
2025-08-07 12:18:33 +0600

    verifyToken

--- index.js
+++ index.js
@@ -12,19 +12,20 @@ app.use(
       "https://job-portal-f18ce.firebaseapp.com",
       "https://job-portal-f18ce.web.app",
       "http://localhost:5173",
-    ], 
+    ],
     credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(express.json());
-const logger = (req,res,next)=>{
-  console.log('insite the logger')
+const logger = (req, res, next) => {
+  console.log("insite the logger");
+  next();
+};
+const verifyTiken = (req, res, next) => {
+  console.log("verify token ");
   next()
-}
-const verifyTiken=(req,res,next)=>{
-console.log()
-}
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -82,7 +83,8 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/job-application",logger, async (req, res) => {
+    app.get("/job-application", logger,verifyTiken, async (req, res) => {
+      console.log("on inside the api callback");
       const email = req.query.email;
       const query = { applicant_email: email };
 
